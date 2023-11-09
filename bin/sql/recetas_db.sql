d360e31532ec9ffceb75cdb51dae2e5cb815f9d8
Luis Kinn DC <luiskinndc@gmail.com>
2023-11-09 16:20:00 -0500

    LuisKinnDC

--- bin/sql/recetas_db.sql
+++ bin/sql/recetas_db.sql
@@ -1,50 +1,66 @@
-CREATE TABLE `usuarios` (
-  `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  `nombre` VARCHAR(255),
-  `correo_electronico` VARCHAR(255),
-  `contrasena` VARCHAR(255)
+CREATE TABLE Receta (
+  id_receta INT PRIMARY KEY,
+  nombre VARCHAR(255),
+  descripcion TEXT,
+  tiempo_de_preparacion INT,
+  instruccion_de_preparacion TEXT,
+  dificultad INT,
+  id_categoria INT
 );
 
-INSERT INTO `usuarios` (`nombre`, `correo_electronico`, `contrasena`)
-VALUES ('clinton', 'clinton@example.com', '2023elp');
-
-
-CREATE TABLE `recetas` (
-  `id` INT PRIMARY KEY,
-  `titulo` VARCHAR(255),
-  `descripcion` TEXT,
-  `tiempo_preparacion` INT,
-  `tiempo_coccion` INT,
-  `porciones` INT,
-  `dificultad` VARCHAR(255),
-  `imagen` VARCHAR(255),
-  `usuario_id` INT
+CREATE TABLE Categoria (
+  id_categoria INT PRIMARY KEY,
+  nombre VARCHAR(255)
 );
 
-CREATE TABLE `ingredientes` (
-  `id` INT PRIMARY KEY,
-  `nombre` VARCHAR(255),
-  `cantidad` VARCHAR(255)
+CREATE TABLE Ingrediente (
+  id_ingrediente INT PRIMARY KEY,
+  nombre VARCHAR(255),
+  medida VARCHAR(255)
 );
 
-CREATE TABLE `recetas_ingredientes` (
-  `id` INT PRIMARY KEY,
-  `receta_id` INT,
-  `ingrediente_id` INT
+CREATE TABLE Ingrediente_receta (
+  id_ingrediente_receta INT PRIMARY KEY,
+  id_receta INT,
+  id_ingrediente INT,
+  cantidad INT
 );
 
-CREATE TABLE `pasos` (
-  `id` INT PRIMARY KEY,
-  `descripcion` TEXT,
-  `orden` INT,
-  `receta_id` INT
+CREATE TABLE Comentarios (
+  id_comentarios INT PRIMARY KEY,
+  id_receta INT,
+  nombre_autor VARCHAR(255),
+  fecha DATE
 );
 
-ALTER TABLE `recetas` ADD FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`);
+CREATE TABLE Usuario (
+  id_usuario INT PRIMARY KEY,
+  nombre VARCHAR(255),
+  contrasena VARCHAR(255),
+  correo_electronico VARCHAR(255)
+);
 
-ALTER TABLE `recetas_ingredientes` ADD FOREIGN KEY (`receta_id`) REFERENCES `recetas` (`id`);
+CREATE TABLE Favorito (
+  id_favorito INT PRIMARY KEY,
+  id_usuario INT,
+  id_receta INT
+);
 
-ALTER TABLE `recetas_ingredientes` ADD FOREIGN KEY (`ingrediente_id`) REFERENCES `ingredientes` (`id`);
+-- relacionando las tablas
+ALTER TABLE Receta
+ADD FOREIGN KEY (id_categoria) REFERENCES Categoria(id_categoria);
 
-ALTER TABLE `pasos` ADD FOREIGN KEY (`receta_id`) REFERENCES `recetas` (`id`);
+ALTER TABLE Ingrediente_receta
+ADD FOREIGN KEY (id_receta) REFERENCES Receta(id_receta);
 
+ALTER TABLE Ingrediente_receta
+ADD FOREIGN KEY (id_ingrediente) REFERENCES Ingrediente(id_ingrediente);
+
+ALTER TABLE Comentarios
+ADD FOREIGN KEY (id_receta) REFERENCES Receta(id_receta);
+
+ALTER TABLE Favorito
+ADD FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario);
+
+ALTER TABLE Favorito
+ADD FOREIGN KEY (id_receta) REFERENCES Receta(id_receta);
--- src/module-info.java
+++ src/module-info.java
@@ -7,6 +7,5 @@
 module GestionRecetasCocina {
 	requires java.desktop;
 	requires java.sql;
-	requires mysql.connector.java;
 	requires java.xml;
 }
