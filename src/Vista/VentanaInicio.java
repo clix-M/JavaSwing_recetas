f58dace2cadbc8fad39baa978d2f36c764cc7ac3
Luis Kinn DC <luiskinndc@gmail.com>
2023-11-10 12:47:32 -0500

    LuisKinnDC
    :)

--- src/Vista/VentanaInicio.java
+++ src/Vista/VentanaInicio.java
@@ -3,24 +3,20 @@ package Vista;
 import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.EventQueue;
-import java.awt.FlowLayout;
-import java.awt.FlowLayout;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.SwingConstants;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.Image;
-
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.ActionEvent;
-import java.awt.Window.Type;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
 
 public class VentanaInicio extends JFrame {
 
@@ -35,7 +31,6 @@ public class VentanaInicio extends JFrame {
 				try {
 					VentanaInicio frame = new VentanaInicio();
 					frame.setVisible(true);
-					frame.setLocationRelativeTo(null);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -114,5 +109,5 @@ public class VentanaInicio extends JFrame {
             }
         });
 	}
-	
+
 }
